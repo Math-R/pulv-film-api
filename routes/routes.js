251ef908ab3646fba178bd72deb90e5141a945a3
Mathieu Richard <mathieu@comeet.in>
2018-04-17 17:38:52 +0200

    requete get user and user/id

--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UserCtrl = require('./user/UserController')
 
 router.route('/user').get(UserCtrl.getAllUsers)
+router.route('/user/:id').get(UserCtrl.getUser)
 
 module.exports = router;
--- routes/user/UserController.js
+++ routes/user/UserController.js
@@ -5,8 +5,18 @@ module.exports= {
     getAllUsers : (req, res) =>{
         connection.query('SELECT * FROM User', function(error,result){
             if (error) throw error
-            res.send(result)            
+            res.status(200).send(result)            
         })
     },
+    getUser: (req, res) =>{
+        const {id} = req.params
+        connection.query(
+            'SELECT * FROM User WHERE id_user ='+id+'',
+            function(error,result){
+                if (error)throw error
+                res.status(200).send(result)
+            }
+        )
+    }
     
 }
