83e7abcbd496c803ed3bb90cb03bd96ab2e6d17e
Mathieu Richard <mathieu@comeet.in>
2018-04-17 10:56:17 +0200

    initialize db connection with .env variables

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ app.use('/api', router)
 app.listen(port)
 
 console.log('je suis sur le port'+port)
+
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     :  process.env.DB_USER,
+    password :  process.env.DB_PASS
+  });
+
+  connection.connect()
+
+  
