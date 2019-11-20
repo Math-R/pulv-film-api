51d669bd14f35ddb6a232ff5cad9b06172381a42
Dorian MARQUES BRAGA <dorian.marques.2e7@gmail.com>
2019-11-20 15:38:59 +0100

    clean route

--- routes/routes.js
+++ routes/routes.js
@@ -5,15 +5,11 @@ const LoginCtrl = require('./login/LoginController');
 const bodyParser = require('body-parser');
 const knex = require('../query.js');
 
-const getMovies = (req, res, next) => {
-    knex.select().from('movies').then(result => res.send(result))
-        .catch(next)
-}
 //Routes
 //Login
 // router.route('/login').post(LoginCtrl.login);
 //User routes
-router.route('/movies').get(getMovies);
+router.route('/movies').get(UserCtrl.getAllUsers);
 
 
 
--- routes/user/UserController.js
+++ routes/user/UserController.js
@@ -1,12 +1,9 @@
 const knex = require('../../query.js')
 
 module.exports= {
-    getAllUsers : (req, res) =>{
-        /*connection.query('SELECT * FROM User', function(error,result){
-            if (error) throw error
-            res.status(200).send(result)
-        })*/
-        res.status(200).send(knex.select().from('movies'))
+    getAllUsers : (req, res, next) =>{
+        knex.select().from('movies').then(result => res.send(result))
+            .catch(next)
     },
 }
 
